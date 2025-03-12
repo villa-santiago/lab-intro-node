90e4e3652b0ccc98363c35e10ece3107ad39f3a2
Santiago Villa <code@santiagovilla.it>
2025-03-12 23:19:45 +0100

    completed lab

--- index.js
+++ index.js
@@ -1,17 +1,52 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = this.items.length;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items = [...this.items, item].sort((a,b) => a-b);
+    this.length = this.items.length;
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (pos < 0 || pos >= this.length){
+      throw new Error("out of bounds");
+    }
+    return this.items[pos]
+  }
 
-  max() {}
+  max() {
+    if (this.items.length === 0){
+      throw new Error("list is empty");
+    }
+    return Math.max(...this.items)
+  }
 
-  min() {}
+  min() {
+    if (this.items.length === 0){
+      throw new Error("list is empty");
+    }
+    return Math.min(...this.items)
+  }
 
-  sum() {}
+  sum() {
+    let result = 0;
+    if (this.items.length === 0){
+      return 0;
+    }
+    for (let i = 0; i < this.items.length; i++){
+      result += this.items[i];
+    }
+    return result;
+  }
 
-  avg() {}
+  avg() {
+    if (this.items.length === 0){
+      throw new Error("list is empty");
+    }
+    return this.items.reduce((acc, num) => acc + num, 0) / this.items.length;
+  }
 }
 
 module.exports = SortedList;
